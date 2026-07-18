884868773f80f25b3ea5abaa1de77ce1fb24ad50
maltmym52-art <maltmym52@gmail.com>
2026-07-19 00:57:26 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,18 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    build: {
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
